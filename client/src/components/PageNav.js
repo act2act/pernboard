f530bce58b98444262aadab51832589a8ef8eb76
act2act <pcyeop@gmail.com>
2024-07-19 10:30:58 +0900

    Modify prev, next function of page navigation bar

--- client/src/components/PageNav.js
+++ client/src/components/PageNav.js
@@ -1,9 +1,16 @@
 function PageNav({ currentPage, totalPages }) {
+    const previousPage = (currentPage) => {
+        if (currentPage > 1) {
+            return currentPage - 1;
+        } else {
+            return currentPage;
+        }
+    } 
     return(
         <nav aria-label="Page navigation">
             <ul className="inline-flex -space-x-px text-sm">
                 <li>
-                    <a href={Math.max(1, currentPage)} class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
+                    <a href={`?page=${previousPage(currentPage)}`} class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
                 </li>
                 {[...Array(totalPages)].map((_, idx) => {
                     return (
@@ -13,7 +20,7 @@ function PageNav({ currentPage, totalPages }) {
                     )
                 })}
                 <li>
-                    <a href={currentPage + 1} class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
+                    <a href={`?page=${currentPage + 1}`} class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
                 </li>
             </ul>
         </nav>
